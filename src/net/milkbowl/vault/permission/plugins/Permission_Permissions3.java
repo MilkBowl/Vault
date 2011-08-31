0d0cf557716cb764fd0a80ecb9e9d5c0060f0e9c
Morgan <mrh9724@lanaddict.com>
2011-08-31 12:28:48 -0400

    Clean up ServerListener for P3

--- src/net/milkbowl/vault/permission/plugins/Permission_Permissions3.java
+++ src/net/milkbowl/vault/permission/plugins/Permission_Permissions3.java
@@ -49,7 +49,7 @@ public class Permission_Permissions3 extends Permission {
         this.plugin = plugin;
         pluginManager = this.plugin.getServer().getPluginManager();
 
-        permissionServerListener = new PermissionServerListener(this);
+        permissionServerListener = new PermissionServerListener();
 
         this.pluginManager.registerEvent(Type.PLUGIN_ENABLE, permissionServerListener, Priority.Monitor, plugin);
         this.pluginManager.registerEvent(Type.PLUGIN_DISABLE, permissionServerListener, Priority.Monitor, plugin);
@@ -82,29 +82,23 @@ public class Permission_Permissions3 extends Permission {
     }
 
     private class PermissionServerListener extends ServerListener {
-        Permission_Permissions3 permission = null;
-
-        public PermissionServerListener(Permission_Permissions3 permission) {
-            this.permission = permission;
-        }
-
         public void onPluginEnable(PluginEnableEvent event) {
-            if (permission.permission == null) {
+            if (permission == null) {
                 Plugin perms = event.getPlugin();
                 if(perms.getDescription().getName().equals("Permissions") && perms.getDescription().getVersion().startsWith("3")) {
                     if (perms.isEnabled()) {
-                        permission.permission = (Permissions) perms;
-                        log.info(String.format("[%s][Permission] %s hooked.", plugin.getDescription().getName(), permission.name));
+                        permission = (Permissions) perms;
+                        log.info(String.format("[%s][Permission] %s hooked.", plugin.getDescription().getName(), name));
                     }
                 }
             }
         }
 
         public void onPluginDisable(PluginDisableEvent event) {
-            if (permission.permission != null) {
+            if (permission != null) {
                 if (event.getPlugin().getDescription().getName().equals("Permissions")) {
-                    permission.permission = null;
-                    log.info(String.format("[%s][Permission] %s un-hooked.", plugin.getDescription().getName(), permission.name));
+                    permission = null;
+                    log.info(String.format("[%s][Permission] %s un-hooked.", plugin.getDescription().getName(), name));
                 }
             }
         }
