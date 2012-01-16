512fb0a36bf24dd8b39e9d2a92e57fbacfa83991
Sleaker <sleaker@comcast.net>
2012-01-16 08:39:28 -0800

    typo herp

--- src/net/milkbowl/vault/economy/plugins/Economy_Essentials.java
+++ src/net/milkbowl/vault/economy/plugins/Economy_Essentials.java
@@ -1,5 +1,5 @@
 /**
-unle * Copyright (C) 2011 Morgan Humes <morgan@lanaddict.com>
+ * Copyright (C) 2011 Morgan Humes <morgan@lanaddict.com>
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
