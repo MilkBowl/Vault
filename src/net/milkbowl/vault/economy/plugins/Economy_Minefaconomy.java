6622c027f4673c1d927f6dc07f223156b0b7d72c
Coniin <sch.johannes@googlemail.com>
2013-07-26 14:13:29 +0200

    Revert "reworked Minefaconomy"
    This reverts commit d5201424441e7bf6dba52d9e937f8f1dd2c8f552.

--- src/net/milkbowl/vault/economy/plugins/Economy_Minefaconomy.java
+++ src/net/milkbowl/vault/economy/plugins/Economy_Minefaconomy.java
@@ -1,5 +1,6 @@
 package net.milkbowl.vault.economy.plugins;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -12,14 +13,17 @@ import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 
 import me.coniin.plugins.minefaconomy.Minefaconomy;
-
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.economy.EconomyResponse.ResponseType;
 
 public class Economy_Minefaconomy implements Economy {
 	private static final Logger log = Logger.getLogger("Minecraft");
 
 	private final String name = "Minefaconomy";
+	private final int fractionalDigits = 2;
+	private final String currencyNamePlural = "Minefacoins";
+	private final String currencyNameSingular = "Minefacoin";
 
 	private Plugin plugin = null;
 	private Minefaconomy economy = null;
@@ -28,192 +32,215 @@ public class Economy_Minefaconomy implements Economy {
 		this.plugin = plugin;
 		Bukkit.getServer().getPluginManager()
 				.registerEvents(new EconomyServerListener(this), plugin);
-		Plugin econ = null;
+
 		// Load Plugin in case it was loaded before
 		if (economy == null) {
-			econ = plugin.getServer().getPluginManager()
+			Plugin econ = plugin.getServer().getPluginManager()
 					.getPlugin("Minefaconomy");
-			log.info("Loading Minefaconomy");	
+			if (econ != null && econ.isEnabled()) {
+				economy = (Minefaconomy) econ;
+				log.info(String.format("[%s][Economy] %s hooked.", plugin
+						.getDescription().getName(), name));
+			}
 		}
-		if (econ != null && econ.isEnabled()) {
-			economy = (Minefaconomy) econ;
-			log.info(String.format("[%s][Economy] %s hooked.", plugin
-					.getDescription().getName(), name));
-		return;
-		}
-		log.info("Error Loading Minefaconomy");
 	}
 
-    public class EconomyServerListener implements Listener {
-        Economy_Minefaconomy economy_minefaconomy = null;
+	public class EconomyServerListener implements Listener {
+		Economy_Minefaconomy economy = null;
 
-        public EconomyServerListener(Economy_Minefaconomy economy_minefaconomy) {
-            this.economy_minefaconomy = economy_minefaconomy;
-        }
+		public EconomyServerListener(Economy_Minefaconomy economy) {
+			this.economy = economy;
+		}
 
-        @EventHandler(priority = EventPriority.MONITOR)
-        public void onPluginEnable(PluginEnableEvent event) {
-            if (economy_minefaconomy.economy == null) {
-                Plugin mfc = plugin.getServer().getPluginManager().getPlugin("Minefaconomy");
+		@EventHandler(priority = EventPriority.MONITOR)
+		public void onPluginEnable(PluginEnableEvent event) {
+			if (economy.economy == null) {
+				Plugin eco = plugin.getServer().getPluginManager()
+						.getPlugin("3co");
 
-                if (mfc != null) {
-                    economy_minefaconomy.economy = (Minefaconomy) economy;
-                    log.info(String.format("[%s][Economy] %s hooked.", plugin.getDescription().getName(), economy_minefaconomy.name));
-                }
-            }
-        }
+				if (eco != null) {
+					economy.economy = (Minefaconomy) eco;
+					log.info(String.format("[%s][Economy] %s hooked.", plugin
+							.getDescription().getName(), economy.name));
+				}
+			}
+		}
 
-        @EventHandler(priority = EventPriority.MONITOR)
-        public void onPluginDisable(PluginDisableEvent event) {
-            if (economy_minefaconomy.economy != null) {
-                if (event.getPlugin().getDescription().getName().equals("Minefaconomy")) {
-                    economy_minefaconomy.economy = null;
-                    log.info(String.format("[%s][Economy] %s unhooked.", plugin.getDescription().getName(), economy_minefaconomy.name));
-                }
-            }
-        }
-    }
+		@EventHandler(priority = EventPriority.MONITOR)
+		public void onPluginDisable(PluginDisableEvent event) {
+			if (economy.economy != null) {
+				if (event.getPlugin().getDescription().getName()
+						.equals("Minefaconomy")) {
+					economy.economy = null;
+					log.info(String.format("[%s][Economy] %s unhooked.", plugin
+							.getDescription().getName(), economy.name));
+				}
+			}
+		}
+	}
 
 	@Override
 	public boolean isEnabled() {
-		return economy != null && economy.isEnabled();
+		if (economy == null) {
+			return false;
+		} else {
+			return economy.isEnabled();
+		}
 	}
 
 	@Override
 	public String getName() {
-		return economy.vaultLayer.getName();
+		return name;
 	}
 
 	@Override
 	public int fractionalDigits() {
-		return economy.vaultLayer.fractionalDigits();
+		return fractionalDigits;
 	}
 
 	@Override
 	public String format(double amount) {
-		return economy.vaultLayer.format(amount);
+		return economy.format(amount);
 	}
 
 	@Override
 	public String currencyNamePlural() {
-		return economy.vaultLayer.currencyNamePlural();
+		return currencyNamePlural;
 	}
 
 	@Override
 	public String currencyNameSingular() {
-		return economy.vaultLayer.currencyNameSingular();
+		return currencyNameSingular;
 	}
 
 	@Override
 	public boolean hasAccount(String playerName) {
-		return economy.vaultLayer.hasAccount(playerName);
+		return economy.hasAccount(playerName);
 	}
 
 	@Override
 	public boolean hasAccount(String playerName, String worldName) {
-		return economy.vaultLayer.hasAccount(playerName);
+		return hasAccount(playerName);
 	}
 
 	@Override
 	public double getBalance(String playerName) {
-		return economy.vaultLayer.getBalance(playerName);
+		return economy.getBalance(playerName);
 	}
 
 	@Override
 	public double getBalance(String playerName, String world) {
-		return economy.vaultLayer.getBalance(playerName);
+		return getBalance(playerName);
 	}
 
 	@Override
 	public boolean has(String playerName, double amount) {
-		return economy.vaultLayer.has(playerName, amount);
+		return economy.has(playerName, amount);
 	}
 
 	@Override
 	public boolean has(String playerName, String worldName, double amount) {
-		return economy.vaultLayer.has(playerName, amount);
+		return has(playerName, amount);
 	}
 
 	@Override
 	public EconomyResponse withdrawPlayer(String playerName, double amount) {
-		return economy.vaultLayer.withdrawPlayer(playerName, amount);
+		if (economy.withdrawPlayer(playerName, amount)){
+			return new EconomyResponse(0, 0,
+					EconomyResponse.ResponseType.SUCCESS, null);
+		}
+		return new EconomyResponse(amount, 0,
+				EconomyResponse.ResponseType.FAILURE, "Not enough Coins!");
 	}
 
 	@Override
 	public EconomyResponse withdrawPlayer(String playerName, String worldName,
 			double amount) {
-		return economy.vaultLayer.withdrawPlayer(playerName, amount);
+		return withdrawPlayer(playerName, amount);
 	}
 
 	@Override
 	public EconomyResponse depositPlayer(String playerName, double amount) {
-		return economy.vaultLayer.depositPlayer(playerName, amount);
+		if (economy.depositPlayer(playerName, amount)){
+			return new EconomyResponse(amount, 0,
+					EconomyResponse.ResponseType.SUCCESS, null);
+		}
+		return new EconomyResponse(amount, 0,
+				EconomyResponse.ResponseType.FAILURE, "depositError!");
 	}
 
 	@Override
 	public EconomyResponse depositPlayer(String playerName, String worldName,
 			double amount) {
-		return economy.vaultLayer.depositPlayer(playerName, amount);
+		return depositPlayer(playerName, amount);
 	}
 
 	@Override
 	public boolean createPlayerAccount(String playerName) {
-		return economy.vaultLayer.createPlayerAccount(playerName);
+		return economy.createPlayerAccount(playerName);
 	}
 
 	@Override
 	public boolean createPlayerAccount(String playerName, String worldName) {
-		return economy.vaultLayer.createPlayerAccount(playerName);
+		return createPlayerAccount(playerName);
 	}
 
 	@Override
 	public boolean hasBankSupport() {
-		return economy.vaultLayer.hasBankSupport();
+		return false;
 	}
 
 	@Override
 	public EconomyResponse createBank(String name, String player) {
-		return economy.vaultLayer.createBank(name, player);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public EconomyResponse deleteBank(String name) {
-		return economy.vaultLayer.deleteBank(name);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public EconomyResponse bankBalance(String name) {
-		return economy.vaultLayer.bankBalance(name);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public EconomyResponse bankHas(String name, double amount) {
-		return economy.vaultLayer.bankHas(name, amount);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public EconomyResponse bankWithdraw(String name, double amount) {
-		return economy.vaultLayer.bankWithdraw(name, amount);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public EconomyResponse bankDeposit(String name, double amount) {
-		return economy.vaultLayer.bankDeposit(name, amount);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public EconomyResponse isBankOwner(String name, String playerName) {
-		return economy.vaultLayer.isBankOwner(name, playerName);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public EconomyResponse isBankMember(String name, String playerName) {
-		return economy.vaultLayer.isBankMember(name, playerName);
+		return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED,
+				"MineFaconomy does not support bank accounts!");
 	}
 
 	@Override
 	public List<String> getBanks() {
-		return economy.vaultLayer.getBanks();
+		return new ArrayList<String>();
 	}
 
 }
