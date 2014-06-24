ccc4f7c97dfa7d5cef8b83c33358fd8a83f6fac3
Nick Minkler <sleaker@gmail.com>
2014-06-24 15:04:11 -0700

    cleanup how we do the build, manually run the publish script instead of letting travis run maven.

--- .utility/do-publish.sh
+++ .utility/do-publish.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-if [[ "$TRAVIS_REPO_SLUG" != "MilkBowl/Vault" || "$TRAVIS_PULL_REQUEST" == "true" || "$TRAVIS_BRANCH" != "master" ]]
+if [[ "$TRAVIS_REPO_SLUG" != "MilkBowl/Vault" ]]
 then
         echo 'Travis can only publish docs for release builds.'
         return 0
 fi
+if [[ "$TRAVIS_PULL_REQUEST" == "true" || "$TRAVIS_BRANCH" != "master" ]]
+then
+	mvn test
+else
+	mvn clean deploy --settings .utility/settings.xml
+fi
 
-mvn clean deploy --settings .utility/settings.xml
 exit $?
