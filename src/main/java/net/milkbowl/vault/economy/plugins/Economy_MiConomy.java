8971dbf1e989af96223193d01b0385d47bfd2cc1
conreptio <conreptio@gmail.com>
2017-06-07 12:58:11 +0200

    Add Licensing

--- src/main/java/net/milkbowl/vault/economy/plugins/Economy_MiConomy.java
+++ src/main/java/net/milkbowl/vault/economy/plugins/Economy_MiConomy.java
@@ -1,18 +1,22 @@
-/* This file is part of Vault.
-
-    Vault is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Vault is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with Vault.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * This file is part of Vault.
+ *
+ * Copyright (c) 2017 Lukas Nehrke
+ *
+ * Vault is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vault is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Vault.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package net.milkbowl.vault.economy.plugins;
 
 import com.gmail.bleedobsidian.miconomy.Main;
