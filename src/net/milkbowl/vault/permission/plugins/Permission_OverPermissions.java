ac7a48f0c005613c77112439e01f8031beb6b38c
montlikadani <montlikada@gmail.com>
2019-11-03 19:02:36 +0100

    Update Permission_OverPermissions.java

--- src/net/milkbowl/vault/permission/plugins/Permission_OverPermissions.java
+++ src/net/milkbowl/vault/permission/plugins/Permission_OverPermissions.java
@@ -45,7 +45,7 @@ public class Permission_OverPermissions extends Permission {
                 overPerms = ((OverPermissions) perms);
                 userManager = overPerms.getUserManager();
                 groupManager = overPerms.getGroupManager();
-                log.info(String.format("[%s][Permission] %s hooked.", new Object[] {plugin.getDescription().getName(), "OverPermissions"}));
+                log.info(String.format("[Permission] %s hooked.", "OverPermissions"));
             }
         }
     }
@@ -204,8 +204,7 @@ public class Permission_OverPermissions extends Permission {
                 Plugin perms = Permission_OverPermissions.this.plugin.getServer().getPluginManager().getPlugin("OverPermissions");
                 if (perms != null) {
                     permission.overPerms = ((OverPermissions) perms);
-                    Permission_OverPermissions.log.info(String
-                            .format("[%s][Permission] %s hooked.", new Object[] {Permission_OverPermissions.this.plugin.getDescription().getName(), "OverPermissions"}));
+                    Permission_OverPermissions.log.info(String.format("[Permission] %s hooked.", "OverPermissions"));
                 }
             }
         }
@@ -216,8 +215,7 @@ public class Permission_OverPermissions extends Permission {
             if ((permission.overPerms != null) &&
                     (event.getPlugin().getDescription().getName().equals("OverPermissions"))) {
                 permission.overPerms = null;
-                Permission_OverPermissions.log.info(String
-                        .format("[%s][Permission] %s un-hooked.", new Object[] {Permission_OverPermissions.this.plugin.getDescription().getName(), "OverPermissions"}));
+                Permission_OverPermissions.log.info(String.format("[Permission] %s un-hooked.", "OverPermissions"));
             }
         }
     }
