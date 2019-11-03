41ddf51a91bd524892e066b73eb7b2ecc31528ac
montlikadani <montlikada@gmail.com>
2019-11-03 19:00:20 +0100

    Update Permission_DroxPerms.java

--- src/net/milkbowl/vault/permission/plugins/Permission_DroxPerms.java
+++ src/net/milkbowl/vault/permission/plugins/Permission_DroxPerms.java
@@ -30,9 +30,9 @@ public class Permission_DroxPerms extends Permission {
             DroxPerms p = (DroxPerms) plugin.getServer().getPluginManager().getPlugin("DroxPerms");
             if (p != null) {
                 API = p.getAPI();
-                log.info(String.format("[%s][Permission] %s hooked.", plugin.getDescription().getName(), "DroxPerms"));
+                log.info(String.format("[Permission] %s hooked.", "DroxPerms"));
                 useOnlySubgroups = p.getConfig().getBoolean("Vault.useOnlySubgroups", true);
-                log.info(String.format("[%s][Permission] Vault.useOnlySubgroups: %s", plugin.getDescription().getName(), useOnlySubgroups));
+                log.info(String.format("[Permission] Vault.useOnlySubgroups: %s", useOnlySubgroups));
             }
         }
 
@@ -46,7 +46,7 @@ public class Permission_DroxPerms extends Permission {
                 Plugin permPlugin = event.getPlugin();
                 if (permPlugin.getDescription().getName().equals("DroxPerms")) {
                     API = ((DroxPerms) permPlugin).getAPI();
-                    log.info(String.format("[%s][Permission] %s hooked.", plugin.getDescription().getName(), name));
+                    log.info(String.format("[Permission] %s hooked.", name));
                 }
             }
         }
@@ -56,7 +56,7 @@ public class Permission_DroxPerms extends Permission {
             if(API != null) {
                 if(event.getPlugin().getDescription().getName().equals("DroxPerms")) {
                     API = null;
-                    log.info(String.format("[%s][Permission] %s un-hooked.", plugin.getDescription().getName(), name));
+                    log.info(String.format("[Permission] %s un-hooked.", name));
                 }
             }
         }
