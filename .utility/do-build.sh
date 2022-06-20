eae599c14430c069a5a64d86d377c953a7cd7fe5
Jack Fitch <55409055+Jack1424@users.noreply.github.com>
2022-06-20 19:58:22 -0400

    another attempt to fix Travis build

--- .utility/do-build.sh
+++ .utility/do-build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-if [[ "${TRAVIS_REPO_SLUG}" != "MilkBowl/Vault" ]]
-then
+if [[ "${TRAVIS_REPO_SLUG}" != "MilkBowl/Vault" ]]; then
         echo 'Travis can only publish docs for release builds.'
         return 0
 fi
-if [[ "${TRAVIS_PULL_REQUEST}" = "true" || "${TRAVIS_BRANCH}" != "master" ]]
-then
+
+if [[ "${TRAVIS_PULL_REQUEST}" == "true" || "${TRAVIS_BRANCH}" != "master" ]]; then
 	mvn test
 else
 	mvn clean deploy --settings .utility/settings.xml
