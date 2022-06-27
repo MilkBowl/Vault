516c0edc96e19d2f4ff16f84b03879f10c54b0dc
Morgan <cerealcable@users.noreply.github.com>
2022-06-27 10:50:08 -0700

    build: use travis for config for conditionals instead of scripts (#893)

--- .utility/build.sh
+++ .utility/build.sh
@@ -4,11 +4,5 @@ then
         echo 'Travis can only publish docs for release builds.'
         return 0
 fi
-if [[ $TRAVIS_PULL_REQUEST == true || "$TRAVIS_BRANCH" != "master" ]]
-then
-	mvn test
-else
-	mvn clean deploy --settings .utility/settings.xml
-fi
-
+mvn test
 exit $?
