8eb60bf578f76f3cf7f8593556b12a5486543c3b
yui-Kitamura <ntgray27@yahoo.co.jp>
2023-02-13 08:23:36 +0900

    jump up to JDK17

---
+++ src/module-info.java
@@ -0,0 +1,28 @@
+module Vault {
+    requires java.logging;
+    requires DroxPerms;
+    requires EssentialsGroupManager;
+    requires KPerms;
+    requires PermissionsBukkit;
+    requires PermissionsEx;
+    requires Privileges;
+    requires SimplyPerms;
+    requires Starburst;
+    requires TotalPermissions;
+    requires VaultAPI;
+    requires Xperms;
+    requires bpermissions;
+    requires bpermissions25;
+    requires bstats.base;
+    requires bstats.bukkit;
+    requires iChat;
+    requires json.simple;
+    requires mChat;
+    requires mChatSuite;
+    requires org.bukkit;
+    requires overpermissions;
+    requires rscPermissions;
+    requires org.json.simple;
+
+    exports net.milkbowl.vault;
+}
